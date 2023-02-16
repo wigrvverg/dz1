6e904af3853ab4b0907f6922acb383f49a0655d1
User <galinagahverdieva@gmail.com>
2023-02-16 12:09:32 +0300

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("12312");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a>b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b>c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a>b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+            }
+        }
+
+
+
---
+++ src/main/java/org/example/dz1_1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class dz1_1 {
+    public static void main(String[] args) {
+
+    }
+}
